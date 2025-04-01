fa19b5a6dd6808c2317062b13c46561b5b2eaa09
whysmx <185018402@qq.com>
2025-04-02 03:08:02 +0800

    切换到静态导出模式解决SSR渲染问题

--- web/next.config.js
+++ web/next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    unoptimized: true
+  },
+  // 设置为静态HTML导出模式
+  output: 'export',
+  // 禁用SSR功能
+  trailingSlash: true
 }
 
 module.exports = nextConfig 
